0a909b9066d2a055f55af56e9f621801dca52696
Çağrı ULAŞ <cagriulas@gmail.com>
2017-02-12 23:41:13 +0300

    Added files

---
+++ make_repo.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+cd arch_mate/
+mkdir -p ../arch-mate-repo/packages
+for i in `cat ../order|xargs` ; do
+echo $i
+cd $i && cp *.pkg.tar.xz ../../arch-mate-repo/packages && cd ..
+done
+cd ../arch-mate-repo/packages
+for package in `ls *.pkg.tar.xz|xargs` ; do
+gpg --detach-sign $package
+done
+cd ..
+repo-add mate-1.17.db.tar.gz ./packages/* -s
