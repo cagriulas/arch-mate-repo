9058034b794b6894b670ea476921ea901ec53673
Çağrı ULAŞ <cagriulas@gmail.com>
2017-03-15 16:20:09 +0300

    removed caja-extensions-common sha1sum fix

--- build.sh
+++ build.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-#fix sha1sum of caja-extensions-common
-sed -i "18 s/123123321/de5d479838ace6e26ce5cbeb0ab31cd8b23d0081/" arch_mate/caja-extensions-common/PKGBUILD
 
 cd arch_mate/
 for i in `cat ../order|xargs` ; do
